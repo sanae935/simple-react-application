c02db1704cb6d2f8973657c39c4d26b0b4fa0127
sanae935 <s.elatmani005@gmail.com>
2024-11-24 21:13:33 -0500

    pushing file

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {useState, useEffect} from "react";
+import "./App.css";
+
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  // Constant with your API Key
+  const apiKey = "d79205b7";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async(searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch(e) {
+      console.error(e)
+    }
+  }
+  
+  // We pass the getMovie function as a prop called moviesearch
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay />
+    </div>
+  );
+}
